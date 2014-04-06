91b3f156cc3cb2f17ed4c319557e04ddf6250a0c
Calvin Yu <calvin@scoutmob.com>
2014-04-06 15:34:10 -0400

    Initial commit; Slack webhook

---
+++ webhooks/slack.rb
@@ -0,0 +1,26 @@
+api_version "1"
+
+info do
+  title "Slack"
+  description "Slack webhooks"
+  contact "me@sourcebender.com"
+  service_provider "slack"
+end
+
+webhook :on_message do
+  summary "Outgoing WebHook"
+  notes "Called on messages sent to Slack"
+
+  method :post
+
+  parameter :token
+  parameter :team_id
+  parameter :channel_id
+  parameter :channel_name
+  parameter :timestamp
+  parameter :user_id
+  parameter :user_name
+  parameter :text
+
+  response_body ''
+end
